49a663940118cc36ef998f0df4387f4909d98315
Roman <nepalroman100@gmail.com>
2024-08-02 23:25:27 +0545

    refactor: online list from homepage removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 "use client";
 import ContactList from "@/components/sections/ContactList";
-import OnlineStatus from "@/components/sections/OnlineStatus";
 import { getUserData, uploadData } from "@/utils/data";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen justify-between p-24">
       <ContactList />
-      <OnlineStatus />
     </main>
   );
 }
